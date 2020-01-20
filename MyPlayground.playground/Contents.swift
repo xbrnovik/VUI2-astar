301be7cd8c884225112adcb884bc514261ec6e78
xbrnovik <xbrnovik@node.mendelu.cz>
2020-01-20 21:36:57 +0100

    Cleanup

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,12 +1,10 @@
 import UIKit
 
-import UIKit
-
 struct Position {
-    let x,y : Int
+    let x, y : Int
 }
 
-class Node: Equatable {
+class Node {
     var g: Int = 0
     var h: Int = 0
     var f: Int = 0
@@ -17,24 +15,23 @@ class Node: Equatable {
         self.parent = parent
         self.position = position
     }
-    
+}
+
+extension Node: Equatable {
     static func == (lhs: Node, rhs: Node) -> Bool {
         (lhs.position.x == rhs.position.x) && (lhs.position.y == rhs.position.y)
     }
 }
 
 class Algorithms {
-    
     func astar(maze: [[Int]], start: Position, end: Position) -> [Position]? {
         let startNode = Node(position: start)
         let endNode = Node(position: end)
         var openList: [Node] = []
         var closedList: [Node] = []
         openList.append(startNode)
-        var astarPath: [Position]? = nil
         
         while openList.count > 0 {
-            
             guard
                 let currentNode = openList.min(by: { $0.f < $1.f }),
                 let currentIndex = openList.firstIndex(of: currentNode)
@@ -45,18 +42,16 @@ class Algorithms {
             openList.remove(at: currentIndex)
             closedList.append(currentNode)
             
-            if currentNode == endNode {
+            guard currentNode != endNode else {
                 var path: [Position] = []
                 var current: Node? = currentNode
                 while current != nil {
                     path.append(current!.position)
                     current = current?.parent
                 }
-                astarPath = path.reversed()
-                break
+                return path.reversed()
             }
 
-            var children: [Node] = []
             let adjacentSquares = [
                 Position(x: 0, y: -1),
                 Position(x: 0, y: 1),
@@ -67,63 +62,60 @@ class Algorithms {
                 Position(x: 1, y: -1),
                 Position(x:1, y:1)
             ]
-            
-            for newPosition in adjacentSquares {
-                let nodePosition = Position(x: currentNode.position.x + newPosition.x,
-                                            y: currentNode.position.y + newPosition.y)
-                
-                if ( nodePosition.x > (maze.count-1) || nodePosition.x < 0 || nodePosition.y > (maze[maze.count-1].count-1) || nodePosition.y < 0) {
-                    continue
+                        
+            let children: [Node] = adjacentSquares.compactMap {
+                let nodePosition = Position(x: currentNode.position.x + $0.x, y: currentNode.position.y + $0.y)
+                guard
+                    nodePosition.x <= (maze.count-1),
+                    nodePosition.x >= 0,
+                    nodePosition.y <= (maze[maze.count-1].count-1),
+                    nodePosition.y >= 0,
+                    maze[nodePosition.x][nodePosition.y] == 0
+                else {
+                    return nil
                 }
-                
-                if maze[nodePosition.x][nodePosition.y] != 0 {
-                    continue
-                }
-                
-                let newNode = Node(parent: currentNode, position: nodePosition)
-                children.append(newNode)
+                return Node(parent: currentNode, position: nodePosition)
             }
             
-            for child in children {
-                for closedChild in closedList {
-                    if child == closedChild {
-                        continue
+            children.forEach { child in
+                closedList.forEach { (closedChild) in
+                    guard child == closedChild else {
+                        return
                     }
                 }
                 child.g = currentNode.g + 1
-                let a = child.position.x - endNode.position.x
-                let b = child.position.y - endNode.position.y
-                child.h = Int(pow(Double(a),Double(2))) + Int(pow(Double(b),Double(2)))
+                child.h = pow2(child.position.x - endNode.position.x) + pow2(child.position.y - endNode.position.y)
                 child.f = child.g + child.h
-                
-                for openNode in openList {
-                    if (child == openNode) && (child.g > openNode.g) {
-                        continue
+                openList.forEach { (openNode) in
+                    guard (child == openNode) && (child.g > openNode.g) else {
+                        return
                     }
                 }
                 openList.append(child)
             }
-            
         }
-        return astarPath
-        
+        return nil
+    }
+    
+    private func pow2(_ a: Int) -> Int {
+        return Int(pow(Double(a),Double(2)))
     }
 }
 
 
-let maze = [[0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+let maze = [
+    [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+]
 
 let start = Position(x: 0, y: 0)
 let end = Position(x: 7, y: 6)
-
-let starPath = Algorithms().astar(maze: maze, start: start, end: end)
-print(starPath)
+let astarPath = Algorithms().astar(maze: maze, start: start, end: end)
